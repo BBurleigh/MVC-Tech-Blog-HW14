f04cfd3b34a375da51c20a7cbfee68e9726ee6f1
Brayden <brayburl224@gmail.com>
2022-12-21 16:42:32 -0500

    created router.get to render blog posts on homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      const blogData = await Blog.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+      });
+
+      const blogPosts = blogData.map((blog) => blog.get({ plain: true }));
+
+      res.render('homepage', {
+        blogPosts,
+        // logged_in: req.session.logged_in
+      });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
